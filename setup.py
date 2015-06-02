fbd6eb5b32bc8c968003d9a387a5d720f41d0190
torpedoallen <torpedoallen@gmail.com>
2015-06-02 03:29:16 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='have-a-rest',
+    version=".".join(map(str, __import__("have_a_rest").__version__)),
+    description='Gracefully describe your api model and documenting',
+    author='Christian Ren',
+    author_email='torpedoallen@gmail.com',
+    url='http://github.com/torpedoallen/have-a-rest',
+    packages=find_packages(),
+    classifiers=[
+    ],
+    license="MIT",
+)
